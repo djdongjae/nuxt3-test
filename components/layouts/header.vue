4cb38ca49c8abac21c83e27ac496e7b25f0e4365
djdongjae <jk47wee@gmail.com>
2025-03-13 13:01:26 +0900

    [#8] fix: 데이터 새로고침 해야만 데이터 불러와지는 문제 해결

--- components/layouts/header.vue
+++ components/layouts/header.vue
@@ -33,17 +33,15 @@
                     </div>
                     <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div class="flex shrink-0 items-center">
-                            <img class="h-8 w-auto"
-                                src="~/assets/img/silveredu_logo.png"
-                                alt="Your Company">
+                            <img class="h-8 w-auto" src="~/assets/img/silveredu_logo2.png" alt="Your Company">
                         </div>
                         <div class="hidden sm:ml-6 sm:block">
                             <div class="flex space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                                <a href="#" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
-                                    aria-current="page">기관</a>
-                                <a href="#"
-                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">계획안</a>
+                                <a href="/"
+                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">홈</a>
+                                <a href="/list"
+                                    class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">기관</a>
                                 <a href="#"
                                     class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">프로그램</a>
                                 <a href="#"
